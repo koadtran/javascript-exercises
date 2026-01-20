ea59b594f22d4c18cbf338afaa394b0a14e0ef9b
Khoa Tran <102198690+koadtran@users.noreply.github.com>
2026-01-20 10:49:43 -0600

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let result = "";
+    for (let i = 1; i <= string.length; i++) {
+        result += string.at(-i);
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = reverseString;

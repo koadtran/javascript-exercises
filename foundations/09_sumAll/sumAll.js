04933d23b1fe8d309e4d5b96587db7c929df4648
Khoa Tran <102198690+koadtran@users.noreply.github.com>
2026-03-16 19:43:39 -0500

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number' || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    let acc = 0;
+    const from = Math.min(a, b);
+    const to = Math.max(a, b);
+    for (let i = from; i <=to; i++) {
+        acc += i;
+    }
+    return acc;
 };
 
 // Do not edit below this line

d14c9111fb4907a22534e1c148c38f85660c9d03
Khoa Tran <102198690+koadtran@users.noreply.github.com>
2026-05-21 12:19:48 -0500

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num;
+    if (typeof num !== "number") {
+        num = parseInt(n);
+    } else {
+        num = n;
+    }
 
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+
+    let first = 0;
+    let second = 1;
+    for (let i = 2; i <= num; i++) {
+        let current = first + second;
+        first = second;
+        second = current;
+    }
+    return second;
 };
 
 // Do not edit below this line

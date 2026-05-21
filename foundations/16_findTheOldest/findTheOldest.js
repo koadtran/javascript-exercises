1a1c898916a6bd6a3c9b8037847a09d9a3421680
Khoa Tran <102198690+koadtran@users.noreply.github.com>
2026-05-21 12:39:58 -0500

    Complete findTheOldest.js

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return (oldestAge < currentAge) ? person : oldest;
+    });
 };
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;

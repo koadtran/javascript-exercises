e8576eca655ef5d59a048dbd8950157e432a8df9
Khoa Tran <102198690+koadtran@users.noreply.github.com>
2026-05-21 12:08:46 -0500

    Complete 13_palindromes.js

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const valid = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const filtered = str.toLowerCase().split('').filter((ch) => valid.includes(ch)).join('');
+    return filtered === filtered.split('').reverse().join('');
 };
 
 // Do not edit below this line
